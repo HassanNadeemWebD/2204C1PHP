db012f4022c0ae7ac898cdb7c0c444d6c9f51fa3
Hassan Nadeem <64688959+HassanNadeemWebD@users.noreply.github.com>
2023-02-11 12:24:06 +0500

    commit

--- insert.php
+++ insert.php
@@ -19,7 +19,8 @@ include "conn.php";
 if (isset($_POST['submit'])) {
     $username = $_POST['username'];    $email = $_POST['email'];    $gender = $_POST['gender'];    $phone = $_POST['phone'];    $message = $_POST['txtMsg'];
     $query = "INSERT INTO `feedback` ( `userName`, `Gender`, `Email`, `Phone`, `Message`) 
-VALUES ( '$username', '$gender', '$email', '$phone', '$message')";    $res = mysqli_query($conn, $query);
+VALUES ( '$username', '$gender', '$email', '$phone', '$message')";   
+ $res = mysqli_query($conn, $query);
 
     if ($res) {
         echo "Data Inserted !";
---
+++ loginsystem/config.php
@@ -0,0 +1,22 @@
+<?php
+
+$host = "localhost";
+$username = "root";
+$password = "";
+$db = "2204C1";
+
+$conn = mysqli_connect($host , $username,$password,$db);
+
+// if($conn){
+
+// echo "connetion established";
+
+
+// }else{
+
+//     die("Error");
+// }
+
+
+
+?>
